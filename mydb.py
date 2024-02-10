3d5620614849075bc25c2f4665d45bb73ff2ceae
tmbumd <82411110+tmbumd@users.noreply.github.com>
2024-02-09 20:47:48 -0500

    Update mydb.py
    take away actual password

--- mydb.py
+++ mydb.py
@@ -10,12 +10,13 @@
 #python manage.py startapp <name of app> (website)
 #winpty python manage.py createsuperuser
 #python manage.py runserver
+#line 19 requires actual password which is redacted
 import mysql.connector
 
 dataBase = mysql.connector.connect(
 	host = 'localhost',
 	user = 'root',
-	passwd = 'Waldspurger22!'
+	passwd = 'fake_password'
 
 	)
 #Prepare cursor object
